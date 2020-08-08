60d91427b3c55d615222a121df7597acd0552e3e
abhijit shaw <36149425+shaw2018@users.noreply.github.com>
2020-08-08 17:39:13 +0530

    Update dmass.py

--- dmass.py
+++ dmass.py
@@ -1,47 +1,59 @@
 import discord
-from discord.ext.commands import bot
-from discord import game
 from discord.ext import commands
-import asyncio
 import platform
-import colorsys
 import random
 import time
 
-client = commands.Bot(command_prefix = '+', case_insensitive=True)
-Client = discord.client
-Clientdiscord = discord.Client()
+bot = commands.Bot(command_prefix='+', case_insensitive=True)
 
-@client.event
+@bot.event
 async def on_ready():
-    print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
-    print('--------')
+    print(f'Logged in as {bot.user.name}(ID: +{bot.user.id}) |'
+          f'Connected to {str(len(bot.guilds))} servers |'
+          f'Connected to {str(len(set(bot.get_all_members())))} users')
     print('--------')
     print('CREATED AND HOSTED BY ShaW')
 
-@client.command(pass_context = True)
-@commands.has_permissions(kick_members=True)     
+
+@bot.event
+async def on_command_error(ctx, error):
+    # Ignore these errors:
+    ignored = (
+        commands.CommandNotFound, commands.UserInputError, commands.BotMissingPermissions, commands.MissingPermissions, discord.errors.Forbidden, commands.CommandInvokeError, commands.MissingRequiredArgument)
+    if isinstance(error, ignored):
+        return
+
+
+@bot.command(pass_context=True)
+@commands.has_permissions(kick_members=True)
 async def userinfo(ctx, user: discord.Member):
-    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-    embed = discord.Embed(title="{}'s info".format(user.name), description="Here's what I could find.", color = discord.Color((r << 16) + (g << 8) + b))
-    embed.add_field(name="Name", value=user.name, inline=True)
-    embed.add_field(name="ID", value=user.id, inline=True)
-    embed.add_field(name="Status", value=user.status, inline=True)
-    embed.add_field(name="Highest role", value=user.top_role)
-    embed.add_field(name="Joined", value=user.joined_at)
-    embed.set_thumbnail(url=user.avatar_url)
-    await client.say(embed=embed)
-    
+    try:
+        embed = discord.Embed(title="{}'s info".format(user.name),
+                              description="Here's what I could find.",
+                              color=discord.Colour.dark_red())
+
+        embed.add_field(name="Name", value=user.name, inline=True)
+        embed.add_field(name="ID", value=user.id, inline=True)
+        embed.add_field(name="Status", value=user.status, inline=True)
+        embed.add_field(name="Highest role", value=user.top_role)
+        embed.add_field(name="Joined", value=user.joined_at)
+        embed.set_thumbnail(url=user.avatar_url)
+
+        await ctx.send(embed=embed)
+    except:
+        await ctx.send("Missing Requrired Args")
+
+
 @commands.has_permissions(administrator=True)
-@client.command(pass_context = True)
+@bot.command(pass_context=True)
 async def send(ctx, *, content: str):
-        for member in ctx.message.server.members:
-            try:
-                await client.send_message(member, content)
-                await client.say("DM Sent To : {} :white_check_mark:  ".format(member))
-            except:
-                print("can't")
-                await client.say("DM can't Sent To : {} :x: ".format(member))
+    for member in ctx.guild.members:
+        c = await member.create_dm()
+        try:
+            await c.send(content)
+            await ctx.send("Message Sent to Targets")
+        except:
+            await ctx.send("DM can't send to : {} :x: ".format(member))
 
 
-client.run("NzQxNTk4MTMyNTA0MzYzMDIw.Xy55FQ.1V940d4C7FKa2TEq2BnJ-mxA3I4")                
+bot.run("NzQxNTk4MTMyNTA0MzYzMDIw.Xy55FQ.1V940d4C7FKa2TEq2BnJ-mxA3I4")
